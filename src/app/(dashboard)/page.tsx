ab3f7247b0577318d6763726cd07a391df91c121
zombyacoff <zombyacoff@gmail.com>
2025-02-06 04:09:35 +0300

    one more deploy fix

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./dashboard.module.scss";
+import styles from "./home.module.scss";
 import { Background } from "@/components/ui/AnimatedBackground";
 
 function titleSpan(text: string) {
